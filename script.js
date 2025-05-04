c225a882f1e8299b671737cfeb1b339a1da2bf57
RansikaSeniya <bandararansika308@gmail.com>
2025-05-04 16:38:47 +0530

    complete the js part

---
+++ script.js
@@ -0,0 +1,48 @@
+// Random Number Guess
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+console.log(randomNumber);
+
+const attempts = 10;
+let guessCount = 0;
+
+const guessingNumber = document.getElementById('guessingNumber');
+const guessingForm = document.getElementById('guessingForm');
+const hint = document.getElementById('hint');
+const attemptsElement = document.getElementById('attempts');
+const history = document.getElementById('history');
+
+let guessHistory = [];
+
+guessingForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const guess =parseInt(guessingNumber.value);
+    checkGuess(guess);
+    guessingNumber.value = '';
+});
+
+function checkGuess(guess){
+    guessCount++;
+    guessHistory.push(guess);
+
+    attemptsElement.textContent = `You have ${attempts - guessCount} attempts left`;
+    
+    if (guess === randomNumber) {
+        guessingForm.style.display = 'none';
+        hint.textContent = 'You Guessed The Correct Number';
+        attemptsElement.textContent = `You took ${guessCount} attempts To Guess The Number`;
+        history.textContent = `Your Guess History: ${guessHistory.join(', ')}`;
+    } else if (guess < randomNumber) {
+        hint.textContent = 'Your Guess Is Too Low! Try Again..';
+        // attemptsElement.textContent = `You have ${attempts - guessCount} attempts left`;
+        history.textContent = `Your Guess History: ${guessHistory.join(', ')}`;
+    } else if (guess > randomNumber) {
+        hint.textContent = 'Your Guess Is Too High! Try Again..';
+        // attemptsElement.textContent = `You have ${attempts - guessCount} attempts left`;
+        history.textContent = `Your Guess History: ${guessHistory.join(', ')}`;
+    }
+
+    if (guessCount === attempts) {
+        guessingForm.style.display = 'none';
+        hint.textContent = 'You Failed To Guess The Number! Refresh The Page To Try Again';
+    }
+}
